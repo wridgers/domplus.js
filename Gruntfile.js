8144a378676444ce303674109db4bdaf46adbcff
Will <will@mindfuzz.net>
2013-08-15 22:52:55 +0100

    Reconfig'd Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,29 +3,39 @@ module.exports = function(grunt) {
   // config
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+    concat: {
       options: {
-        banner: '/*! <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd") %> - https://github.com/wridgers/domplus.js */\n'
+        separator: ';'
       },
-      build: {
-        src: 'src/domplus.js',
-        dest: 'build/domplus.min.js'
+      dist: {
+        src: ['src/core.js'],
+        dest: 'build/domplus-<%= pkg.version %>.js'
       }
     },
     jshint: {
       files: ['src/*.js']
     },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd") %> - https://github.com/wridgers/domplus.js */\n'
+      },
+      build: {
+        src: 'build/domplus-<%= pkg.version %>.js',
+        dest: 'build/domplus-<%= pkg.version %>.min.js'
+      }
+    },
     watch: {
       files: ['src/*.js'],
-      tasks: ['jshint', 'uglify']
+      tasks: ['jshint', 'concat', 'uglify']
     }
   });
 
   // plugins
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // default
-  grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'watch']);
 };
